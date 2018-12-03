4b0ea6efb479acdc7e8a8931aacc876df0358d30
Joris Lodewijks <joris.lodewijks@hotmail.com>
2018-12-03 13:51:56 +0100

    Dynamic List Progress

--- Website/javascript.js
+++ Website/javascript.js
@@ -9,14 +9,33 @@ var layers = [
 	{name:"Unsorted", active:true, icon:"none"},
 	{name:"Temperature", active:true, icon:"none"}];
 
+addLayer("Testing", "none");
 
-function changeListText(){
+function updateLayerList(){
+
+	var layerList = document.getElementById("layerList");
+
+	for (var i = 0; i < layerList.children.length; i++) {
+		alert(layerList.getChild(i));
+	}
 
 	for (var i = 0; i < layers.length; i++) {
-		var node = document.createElement("LI");
-		var textnode = document.createTextNode(layers[i].name);
-		node.appendChild(textnode);
-		document.getElementById("items").appendChild(node);
+		
+		var newItem = document.createElement("li");
+		var itemValue = document.createTextNode(layers[i].name);
+
+		newItem.appendChild(itemValue);
+		layerList.appendChild(newItem);
 	}
 	
+}
+
+function addLayer (layerName, iconType) {
+
+	var element = {};
+	element.name = layerName;
+	element.active = true;
+	element.icon = iconType;
+	layers.push(element)
+
 }
